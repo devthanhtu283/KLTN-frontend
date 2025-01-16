633ff6c3ac9045d5724de0863345dc07fea8688c
devthanhtu283 <devthanhtu283@gmail.com>
2025-01-16 22:54:59 +0700

    aa

--- quiz/quiz/src/main.ts
+++ quiz/quiz/src/main.ts
@@ -5,3 +5,22 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+//commit test
+//commit test 2
+//commit test
+//commit test 2
+
+//commit test
+//commit test 2
+//commit test
+//commit test 2
+//commit test
+//commit test 2
+//commit test 2
+//commit test
+//commit test 2
+
+//commit test 2
+//commit test
+//commit test 2
+
